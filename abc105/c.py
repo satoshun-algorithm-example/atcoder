8d434d8690fb2e051f8ef5873512306ca4f181a4
SatoShun <shun.sato1@gmail.com>
2018-10-30 00:35:45 +0000

    fix abc105 c

--- abc105/c.py
+++ abc105/c.py
@@ -1,12 +1,13 @@
 def base2_number(n):
     result = ""
     while True:
-        print(n)
         rem = n % 2
-        n = n // 2
+        if rem:
+            n -= 1
+        n = n // -2
         result += str(abs(rem))
         if n == 0:
             break
-    return int(result)
+    return int(result[::-1])
 n = int(input())
 print(base2_number(n))
