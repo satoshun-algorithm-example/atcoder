6d09b16f0701813fb60a77b245936e21c1829dee
SatoShun <shun.sato1@gmail.com>
2018-11-05 22:52:36 +0000

    ruined

--- abc108/b.py
+++ abc108/b.py
@@ -1,19 +1,18 @@
 def ruined_square(x1, y1, x2, y2):
-    if x1 == x2 and y1 > y2:
-        d = abs(y1 - y2)
-        return (x1 + d) + ' ' + y2 + ' ' + (x1 + d) + ' ' + y1
-    if x1 == x2 and y1 < y2:
-        d = abs(y1 - y2)
-        return _f(x1 - d, y2, x1 - d, y1)
-    if x1 > x2 and y1 == y2:
-        d = abs(x1 - x2)
-        return x2 + ' ' + (y2 - d) + ' ' + x1 + ' ' + (y2 - d)
-    if x1 < x2 and y1 == y2:
-        d = abs(x1 - x2)
-        return x2 + ' ' + (y2 + d) + ' ' + x1 + ' ' + (y2 + d)
+    x = x2
+    y = y2
+    dx = x2 - x1
+    dy = y2 - y1
 
-def _f(x3, y3, x4, y4):
-    return '{} {} {} {}'.format(x3, y3, x4, y4)
+    dx, dy = -dy, dx
+    x += dx
+    y += dy
+    print("{} {} ".format(x, y), end = "")
+
+    dx, dy = -dy, dx
+    x += dx
+    y += dy
+    print("{} {}".format(x, y))
 
 x1, y1, x2, y2 = map(int, input().split())
-print(ruined_square(x1, y1, x2, y2))
+ruined_square(x1, y1, x2, y2)
