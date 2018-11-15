0504252a96fc38492df157a45c6969f8bbbc7e3b
SatoShun <shun.sato1@gmail.com>
2018-11-15 00:44:43 +0000

    infinite

--- abc106/c.py
+++ abc106/c.py
@@ -5,10 +5,9 @@ k = int(input())
 
 ss = str(s)
 for i in range(len(ss)):
-    if ss[i] != "0":
+    if ss[i] != "1":
+        print(ss[i])
         break
     if k - 1 == i:
         print(1)
-        exit(0)
-
-print(str(s)[0])
+        break
