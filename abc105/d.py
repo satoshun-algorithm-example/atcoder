f5698dcfe244f3b5788cbd048898ee48b735bd74
SatoShun <shun.sato1@gmail.com>
2018-11-20 00:47:25 +0000

    candy

--- abc105/d.py
+++ abc105/d.py
@@ -1,14 +1,15 @@
+from collections import defaultdict
+
 n, m = map(int, input().split())
 alist = list(map(int, input().split()))
 
-ca = {0: 0}
-for i, a in enumerate(alist):
-    ca[i+1] = sum(alist[0:i+1]) % m
-
 c = 0
-for i in range(1, n+1):
-    for j in range(i, n+1):
-        s = ca[j] - ca[i-1]
-        if s % m == 0:
-            c += 1
+cnt = defaultdict(int)
+cnt[0] = 1
+s = 0
+for i in range(n):
+    s += alist[i]
+    c += cnt[s%m]
+    cnt[s%m] += 1
+
 print(c)
