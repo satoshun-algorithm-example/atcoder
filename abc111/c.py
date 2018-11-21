bb38e0d66162929e0ab0949039bfa67505fa8873
SatoShun <shun.sato1@gmail.com>
2018-11-21 23:55:30 +0000

    abababab

--- abc111/c.py
+++ abc111/c.py
@@ -1,41 +1,24 @@
+from collections import Counter
+
 n = int(input())
 vs = input().split()
 
-vs1 = [vs[i] for i in range(0, len(vs), 2)]
-vs2 = [vs[i] for i in range(1, len(vs), 2)]
+xs = [v for v in vs[::2]]
+x = Counter(xs).most_common()
 
-m1 = 0
-i1 = -1
-m2 = 0
-i2 = -1
-ccc = 0
-for value in vs1:
-    ccc += 1
-    count = vs1.count(value)
-    if count > m1:
-        i2 = i1
-        m2 = m1
-        i1 = value
-        m1 = count
+ys = [v for v in vs[1::2]]
+y = Counter(ys).most_common()
 
-m3 = 0
-i3 = -1
-m4 = 0
-i4 = -1
-ccc2 = 0
-for value in vs2:
-    ccc2 += 1
-    count = vs2.count(value)
-    if count > m3:
-        i4 = i3
-        m4 = m3
-        i3 = value
-        m3 = count
-
-if i1 != i3:
-    print((ccc - m1) + (ccc2 - m3))
+if x[0][0] != y[0][0]:
+    d = n - x[0][1] - y[0][1]
+    print(d)
 else:
-    if m2 > m4:
-        print((ccc - m2) + (ccc2 - m3))
+    if len(x) == 1:
+        print(n // 2)
     else:
-        print((ccc - m1) + (ccc2 - m4))
+        d1 = n - x[0][1] - y[1][1]
+        d2 = n - x[1][1] - y[0][1]
+        if d1 > d2:
+            print(d2)
+        else:
+            print(d1)
