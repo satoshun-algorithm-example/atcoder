3de131920278e33adfd901da607507837c8e67cb
SatoShun <shun.sato1@gmail.com>
2018-11-27 23:16:00 +0000

    We Love ABC but

--- abc104/d.py
+++ abc104/d.py
@@ -1,14 +1,36 @@
-s = input()
-leng = len(s)
-c = 0
-for i in range(leng):
-    if s[i] not in 'A?':
+st = input()
+
+s = 0
+for i in range(len(st)):
+    p = st[i]
+    if not (p == 'B' or p == '?'):
         continue
-    for j in range(i + 1, leng):
-        if s[j] not in 'B?':
-            continue
-        for k in range(j + 1, leng):
-            if s[k] not in 'C?':
-                continue
-            c += 1
-print(c)
+
+    # ABC
+    a1 = 0
+    q1 = 0
+    for l in range(0, i+1):
+        if st[l] == 'A':
+            a1 += 1
+        if st[l] == '?':
+            q1 += 1
+
+    b2 = 0
+    q2 = 0
+    for r in range(i+1, len(st)):
+        if st[r] == 'C':
+            b2 += 1
+        if st[r] == '?':
+            q2 += 1
+    s += a1 * b2 * 3**(q1+q2)
+
+    # AB?
+    s += a1 * q2 * 3**(q1+(q2-1))
+
+    # ?BC
+    s += q1 * b2 * 3**((q1-1)+q2)
+
+    # ?B?
+    s += q1 * q2 * 3**((q1-1)+(q2-1))
+
+print(s)
