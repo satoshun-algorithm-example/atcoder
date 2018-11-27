96093524ef6d97107169a0ab1daeb0e4d5bb58fd
SatoShun <shun.sato1@gmail.com>
2018-11-27 23:27:23 +0000

    We Love ABC TLE

--- abc104/d.py
+++ abc104/d.py
@@ -1,4 +1,5 @@
 st = input()
+wa = 10**9 + 7
 
 s = 0
 for i in range(len(st)):
@@ -9,7 +10,7 @@ for i in range(len(st)):
     # ABC
     a1 = 0
     q1 = 0
-    for l in range(0, i+1):
+    for l in range(0, i):
         if st[l] == 'A':
             a1 += 1
         if st[l] == '?':
@@ -22,15 +23,17 @@ for i in range(len(st)):
             b2 += 1
         if st[r] == '?':
             q2 += 1
+
+    # ABC
     s += a1 * b2 * 3**(q1+q2)
 
     # AB?
     s += a1 * q2 * 3**(q1+(q2-1))
 
-    # ?BC
+    # # ?BC
     s += q1 * b2 * 3**((q1-1)+q2)
 
-    # ?B?
-    s += q1 * q2 * 3**((q1-1)+(q2-1))
+    # # ?B?
+    s = (s + q1 * q2 * 3**((q1-1)+(q2-1))) % wa
 
-print(s)
+print(int(s))
