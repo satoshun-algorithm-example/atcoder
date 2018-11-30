da7292145f969ca345c57685aae0903cfcd7fd0a
SatoShun <shun.sato1@gmail.com>
2018-11-30 00:55:25 +0000

    we love ABC to O(N) but TLE

--- abc104/d.py
+++ abc104/d.py
@@ -2,27 +2,34 @@ st = input()
 wa = 10**9 + 7
 
 s = 0
-for i in range(len(st)):
+
+a1 = 0
+q1 = 0
+
+b2 = st[1:].count('C')
+q2 = st[1:].count('?')
+
+for i in range(1, len(st)):
+    bp = st[i-1]
     p = st[i]
+    if p == 'A':
+        pass
+    if p == 'B':
+        pass
+    if p == 'C':
+        b2 -= 1
+    if p == '?':
+        q2 -= 1
+    if bp == 'A':
+        a1 += 1
+    if bp == 'B':
+        pass
+    if bp == 'C':
+        pass
+    if bp == '?':
+        q1 += 1
     if not (p == 'B' or p == '?'):
         continue
-
-    # ABC
-    a1 = 0
-    q1 = 0
-    for l in range(0, i):
-        if st[l] == 'A':
-            a1 += 1
-        if st[l] == '?':
-            q1 += 1
-
-    b2 = 0
-    q2 = 0
-    for r in range(i+1, len(st)):
-        if st[r] == 'C':
-            b2 += 1
-        if st[r] == '?':
-            q2 += 1
 
     # ABC
     s += a1 * b2 * 3**(q1+q2)
