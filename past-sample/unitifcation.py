091ef3b6016daa6c5404006bbe4011008c979238
SatoShun <shun.sato1@gmail.com>
2019-12-13 11:57:14 +0000

    unitifcation

--- past-sample/unitifcation.py
+++ past-sample/unitifcation.py
@@ -2,18 +2,15 @@ def main():
     s = input()
 
     c = 0
-    while len(s) >= 2:
-        is_match = False
-
-        for i in range(len(s) - 1):
-            if s[i] != s[i + 1]:
-                s = s[:i] + s[i + 2:]
-                c += 2
-                is_match = True
-                break
-
-        if not is_match:
-            break
+    i = 0
+    while len(s) > i + 1:
+        if s[i] != s[i + 1]:
+            s = s[:i] + s[i + 2:]
+            c += 2
+            if i != 0:
+                i -= 1
+            continue
+        i += 1
 
     print(c)
 
