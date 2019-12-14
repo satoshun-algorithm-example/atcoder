169d59d9d3332b40d4ba6f16349cdcc10648c8ed
SatoShun <shun.sato1@gmail.com>
2019-12-14 05:24:56 +0000

    sns

--- past20191214/sns.py
+++ past20191214/sns.py
@@ -5,12 +5,17 @@ def main():
     for _ in range(q):
         op = input().split()
         if op[0] == '1':
-            followers[int(op[1]) - 1][int(op[2]) - 1] = 'Y'
+            y = int(op[1]) - 1
+            x = int(op[2]) - 1
+            if x != y:
+                followers[y][x] = 'Y'
+
         if op[0] == '2':
             x = int(op[1]) - 1
             for y in range(n):
-                if followers[y][x] == 'Y':
+                if followers[y][x] == 'Y' and y != x:
                     followers[x][y] = 'Y'
+
         if op[0] == '3':
             y = int(op[1]) - 1
             follower = followers[y].copy()
@@ -18,7 +23,7 @@ def main():
                 if follower[x] == 'Y':
                     y2 = x
                     for x2 in range(n):
-                        if followers[y2][x2] == 'Y':
+                        if followers[y2][x2] == 'Y' and y != x2:
                             followers[y][x2] = 'Y'
 
     for f in followers:
