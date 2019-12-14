0ce648d921754415de44d2e6b3ec3082b5f9abc1
SatoShun <shun.sato1@gmail.com>
2019-12-14 05:02:58 +0000

    duplicated

--- past20191214/duplicated.py
+++ past20191214/duplicated.py
@@ -3,20 +3,24 @@ def main():
     values = [int(input()) for _ in range(n)]
     values.sort()
 
-    removed = None
-    for i in range(n):
-        if values[i] != i + 1:
-            removed = i + 1
+    duplicated = None
+    for i in range(n - 1):
+        if values[i] == values[i + 1]:
+            duplicated = values[i]
             break
 
-    if removed is None:
+    if duplicated is None:
         print("Correct")
         return
 
-    for i in range(n - 1):
-        if values[i] == values[i + 1]:
-            print(str(values[i]) + " " + str(removed))
-            return
+    removed = None
+    values = set(values)
+    for i in range(1, n + 1):
+        if i not in values:
+            removed = i
+            break
+
+    print(str(duplicated) + " " + str(removed))
 
 
 main()
