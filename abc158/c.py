9767a1efdc8385517d284fb9ade1d5f82f3a7df2
SatoShun <shun.sato1@gmail.com>
2020-03-11 09:33:25 +0000

    ABC158 c

--- abc158/c.py
+++ abc158/c.py
@@ -1,18 +1,11 @@
 A, B = map(int, input().split())
 
-a_min = A * 12.5
-a_max = a_min + 12.5
-a_min = int(a_min)
-a_max = int(a_max)
+for i in range(10000):
+    a = int(i * 0.08)
+    b = int(i * 0.1)
 
-b_min = B * 10.0
-b_max = b_min + 10.0
-b_min = int(b_min)
-b_max = int(b_max)
+    if A == a and B == b:
+        print(i)
+        exit(0)
 
-if b_min <= a_min < b_max:
-    print(a_min)
-elif a_min <= b_min < a_max:
-    print(b_min)
-else:
-    print(-1)
+print(-1)
