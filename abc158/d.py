86b3edca65d099db2b45681aaef27be451a15a76
SatoShun <shun.sato1@gmail.com>
2020-03-18 13:05:47 +0000

    string formation

--- abc158/d.py
+++ abc158/d.py
@@ -3,14 +3,25 @@
 S = input()
 Q = int(input())
 
+elements = [None for _ in range(Q * 2 + len(S) * 2)]
+
+head = Q + len(S)
+last = len(S)
+for i, s in enumerate(S):
+    elements[head + i] = s
+
 for _ in range(Q):
     query = input()
     if query[0] == '1':
-        S = S[::-1]
+        elements[head:head + last] = elements[head + last - 1:head - 1:-1]
     else:
         if query[2] == '1':
-            S = query[4] + S
+            head -= 1
+            elements[head] = query[4]
         else:
-            S = S + query[4]
+            elements[head + last] = query[4]
 
-print(S)
+        last += 1
+
+elements = filter(lambda a: a is not None, elements)
+print(''.join(elements))
