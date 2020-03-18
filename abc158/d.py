7d3693b8218f51adb74df6af8bf933188efdbd0a
SatoShun <shun.sato1@gmail.com>
2020-03-18 22:57:57 +0000

    string formation

--- abc158/d.py
+++ abc158/d.py
@@ -1,27 +1,28 @@
 # https://atcoder.jp/contests/abc158/tasks/abc158_d
+from collections import deque
 
 S = input()
 Q = int(input())
 
-elements = [None for _ in range(Q * 2 + len(S) * 2)]
-
-head = Q + len(S)
-last = len(S)
-for i, s in enumerate(S):
-    elements[head + i] = s
-
+is_reverse = False
+text = deque(S)
 for _ in range(Q):
     query = input()
     if query[0] == '1':
-        elements[head:head + last] = elements[head + last - 1:head - 1:-1]
+        is_reverse = not is_reverse
     else:
         if query[2] == '1':
-            head -= 1
-            elements[head] = query[4]
+            if is_reverse:
+                text.append(query[4])
+            else:
+                text.appendleft(query[4])
         else:
-            elements[head + last] = query[4]
+            if is_reverse:
+                text.appendleft(query[4])
+            else:
+                text.append(query[4])
 
-        last += 1
-
-elements = filter(lambda a: a is not None, elements)
-print(''.join(elements))
+if is_reverse:
+    print(''.join(text)[::-1])
+else:
+    print(''.join(text))
