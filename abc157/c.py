5c41d707bd4c939b06421ed8632a8bba4f4efa23
SatoShun <shun.sato1@gmail.com>
2020-03-24 13:18:49 +0000

    c

---
+++ abc157/c.py
@@ -0,0 +1,24 @@
+# https://atcoder.jp/contests/abc157/tasks/abc157_c
+
+N, M = map(int, input().split())
+c = [-1 for _ in range(N)]
+
+for _ in range(M):
+    S, C = map(int, input().split())
+    if c[S - 1] != -1 and c[S - 1] != C:
+        print(-1)
+        exit(0)
+    c[S - 1] = C
+
+if c[0] == 0:
+    print(-1)
+    exit(0)
+
+s = ""
+for i, a in enumerate(c):
+    if i == 0:
+        s += str(max(a, 1))
+    else:
+        s += str(max(a, 0))
+
+print(s)
