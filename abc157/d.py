475123c8e58d6d76c17cfc1227432334f02b777d
SatoShun <shun.sato1@gmail.com>
2020-03-25 10:11:44 +0000

    d

--- abc157/d.py
+++ abc157/d.py
@@ -6,8 +6,8 @@ class UnionFind:
 
     def find(self, a):
         pa = self.parents[a]
-        if pa == a:
-            return pa
+        if pa < 0:
+            return a
         self.parents[a] = self.find(pa)
         return self.parents[a]
 
@@ -17,33 +17,34 @@ class UnionFind:
         if pa == pb:
             return
 
-        self.parents[pa] = pb
+        self.parents[pa] += self.parents[pb]
+        self.parents[pb] = pa
 
     def same(self, a, b):
         return self.find(a) == self.find(b)
 
+    def size(self, a):
+        return -self.parents[self.find(a)]
+
 
 N, M, K = map(int, input().split())
-union = UnionFind([i for i in range(N)])
+union = UnionFind([-1 for i in range(N)])
 
-m = [[i != j for i in range(N)] for j in range(N)]
+m = [0 for _ in range(N)]
 
 for _ in range(M):
     A, B = map(int, input().split())
-    m[A - 1][B - 1] = 0
-    m[B - 1][A - 1] = 0
+    m[A - 1] += 1
+    m[B - 1] += 1
     union.unite(A - 1, B - 1)
+
+for i in range(N):
+    m[i] = union.size(i) - 1 - m[i]
 
 for _ in range(K):
     A, B = map(int, input().split())
-    m[A - 1][B - 1] = 0
-    m[B - 1][A - 1] = 0
+    m[A - 1] -= union.same(A - 1, B - 1)
+    m[B - 1] -= union.same(A - 1, B - 1)
 
-for j, row in enumerate(m):
-    ans = 0
-    for i, e in enumerate(row):
-        if e == 0:
-            continue
-        ans += union.same(i, j)
-
+for j, ans in enumerate(m):
     print(ans, end=' ')
