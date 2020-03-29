12452dcdcd13489a06b88cbd6f52671e8bad6e87
SatoShun <shun.sato1@gmail.com>
2020-03-29 07:34:14 +0000

    e

--- abc157/e.py
+++ abc157/e.py
@@ -1,14 +1,30 @@
 # https://atcoder.jp/contests/abc157/tasks/abc157_e
+import bisect
 
 N = int(input())
 S = list(input())
 Q = int(input())
 
+acc = [[] for _ in range(26)]
+for i in range(N):
+    bisect.insort_left(acc[ord(S[i]) - ord('a')], i)
+
 for _ in range(Q):
     TYPE, Q1, Q2 = input().split()
     if TYPE == '1':
-        Q1 = int(Q1)
-        S[Q1 - 1] = Q2
+        Q1 = int(Q1) - 1
+        del acc[ord(S[Q1]) - ord('a')][bisect.bisect_left(acc[ord(S[Q1]) - ord('a')], Q1)]
+        S[Q1] = Q2
+        bisect.insort_left(acc[ord(S[Q1]) - ord('a')], Q1)
+
     else:
-        Q1, Q2 = int(Q1), int(Q2)
-        print(len(set(S[Q1 - 1:Q2])))
+        Q1, Q2 = int(Q1) - 1, int(Q2)
+        ans = 0
+        for i in range(26):
+            if not acc[i]:
+                continue
+            it = bisect.bisect_left(acc[i], Q1)
+            if it != acc[i][-1] and it < Q2:
+                ans += 1
+
+        print(ans)
