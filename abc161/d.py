77cbf2cc4ba25f5fa11aac7f26389bb5be104507
SatoShun <shun.sato1@gmail.com>
2020-04-04 23:45:04 +0000

    d

--- abc161/d.py
+++ abc161/d.py
@@ -1,39 +1,22 @@
 # https://atcoder.jp/contests/abc161/tasks/abc161_d
-def check(k):
-    prev = k % 10
-    k //= 10
-    while k:
-        cur = k % 10
-        if abs(cur - prev) > 1:
-            return False
-        k //= 10
-        prev = cur
-    return True
-
-
-hijack = {
-    30000: 223221112,
-    25000: 111233456,
-    20000: 76667876,
-    15000: 45544456,
-    10000: 22111101,
-    5000: 4454545,
-    1000: 110111,
-    0: 0
-}
+from collections import deque
 
 K = int(input())
-
-count = 0
 ans = 0
-for (i, j) in hijack.items():
-    if K >= i:
-        count = i
-        ans = j
-        break
+count = 0
+queue = deque()
+for i in range(1, 10):
+    queue.append(i)
 
-while count != K:
-    ans += 1
-    count += check(ans)
+while True:
+    count += 1
+    a = queue.popleft()
+    if count == K:
+        print(a)
+        exit(0)
 
-print(ans)
+    for i in range(-1, 2):
+        d = a % 10 + i
+        if d < 0 or d > 9:
+            continue
+        queue.append(a * 10 + d)
