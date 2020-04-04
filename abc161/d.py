f140ed9db6147ad660a3eb0bb4c9e1db30909e48
SatoShun <shun.sato1@gmail.com>
2020-04-04 13:04:27 +0000

    d

--- abc161/d.py
+++ abc161/d.py
@@ -3,35 +3,17 @@ K = int(input())
 
 count = 0
 ans = 0
-while True:
-    if count == K:
-        break
 
+while count != K:
     ans += 1
 
     # check lunlun
     a = list(map(int, str(ans)))
-    if len(a) == 1:
-        count += 1
-        continue
-
     is_lunlun = True
-    for i in range(len(a)):
-        if i == 0:
-            if abs(a[i] - a[i + 1]) > 1:
-                is_lunlun = False
-                break
-        elif i == len(a) - 1:
-            if abs(a[i] - a[i - 1]) > 1:
-                is_lunlun = False
-                break
-        else:
-            if abs(a[i] - a[i + 1]) > 1:
-                is_lunlun = False
-                break
-            if abs(a[i] - a[i - 1]) > 1:
-                is_lunlun = False
-                break
+    for i in range(len(a) - 1):
+        if abs(a[i] - a[i + 1]) > 1:
+            is_lunlun = False
+            break
 
     count += is_lunlun
 
