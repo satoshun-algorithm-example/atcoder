04091b582bcdb7e3a24ff79e4f7eecb009c958a1
SatoShun <shun.sato1@gmail.com>
2020-04-05 04:42:01 +0000

    e

--- abc161/e.py
+++ abc161/e.py
@@ -2,45 +2,25 @@
 N, K, C = map(int, input().split())
 S = input()
 
-group = []
-batu = 0
-maru = 0
-for i in range(len(S)):
-    if S[i] == 'x':
-        batu += 1
-        if batu == C:
-            batu = 0
-            if maru != 0:
-                group.append(maru)
-                maru = 0
+i = 0
+left = []
+while i < N and len(left) < K:
+    if S[i] == 'o':
+        left.append(i)
+        i += (C + 1)
         continue
-    if batu != 0:
-        maru = maru << batu
-        batu = 0
-    maru = maru << 1
-    maru += 1
+    i += 1
 
-group.append(maru)
+i = N - 1
+right = []
+while i >= 0 and len(right) < K:
+    if S[i] == 'o':
+        right.append(i)
+        i -= (C + 1)
+        continue
+    i -= 1
 
-maximum = []
-for g in group:
-    s = format(g, 'b')
-    m = 0
-    i = 0
-    for c in s:
-        if c == '1' and i <= 0:
-            m += 1
-            i = C
-            continue
-        i -= 1
-
-    maximum.append(m)
-
-if sum(maximum) > K:
-    exit(0)
-
-for i in range(len(group)):
-    g = group[i]
-    m = maximum[i]
-    if format(g, 'b').count("1") != m:
-        print(g, m)
+n = len(left)
+for i in range(n):
+    if left[i] == right[n - i - 1]:
+        print(left[i] + 1)
