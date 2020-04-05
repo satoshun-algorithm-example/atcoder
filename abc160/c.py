746808888674de6a929cb96402d0b45571b00892
SatoShun <shun.sato1@gmail.com>
2020-04-05 08:26:41 +0000

    c

--- abc160/c.py
+++ abc160/c.py
@@ -1,1 +1,13 @@
-#
+# https://atcoder.jp/contests/abc160/tasks/abc160_c
+
+K, N = map(int, input().split())
+A = list(map(int, input().split()))
+
+max_distance = -1
+for i in range(len(A)):
+    if len(A) - 1 == i:
+        max_distance = max(max_distance, (K - A[i]) + A[0])
+    else:
+        max_distance = max(max_distance, A[i + 1] - A[i])
+
+print(K - max_distance)
