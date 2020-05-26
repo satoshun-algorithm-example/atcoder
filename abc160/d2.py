a5140ba37281bc4718a8af221f279588d017f6f4
SatoShun <shun.sato1@gmail.com>
2020-05-26 23:58:09 +0000

    line

--- abc160/d2.py
+++ abc160/d2.py
@@ -2,10 +2,12 @@ def line(N, X, Y):
     X = X - 1
     Y = Y - 1
 
-    queue = [(a, a) for a in range(N - 1)]
-    visited = set()
+    queue = [(a, a + 1) for a in range(N - 1)]
+    queue.append((X, Y))
+    visited = set(queue)
+    print(len(queue))
 
-    for i in range(N - 1):
+    for i in range(N - 2):
         new_queue = []
 
         def add_to_queue(x, y):
@@ -29,15 +31,6 @@ def line(N, X, Y):
             add_to_queue(q[0], q[1] - 1)
             add_to_queue(q[0] + 1, q[1])
             add_to_queue(q[0] - 1, q[1])
-
-            if q[1] == X:
-                add_to_queue(q[0], Y)
-            if q[1] == Y:
-                add_to_queue(q[0], X)
-            if q[0] == X:
-                add_to_queue(Y, q[1])
-            if q[0] == Y:
-                add_to_queue(X, q[1])
 
         print(len(new_queue))
         # print(f"{len(new_queue)} {new_queue}")
