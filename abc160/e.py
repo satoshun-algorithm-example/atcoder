d998c91f236ee43365ede04c31efe649e37a6eab
SatoShun <shun.sato1@gmail.com>
2020-05-28 06:15:44 +0000

    red and green apples

---
+++ abc160/e.py
@@ -0,0 +1,33 @@
+def red_and_green_apples(X, Y, A, B, C, P, Q, R):
+    P.sort(reverse=True)
+    Q.sort(reverse=True)
+    R.sort(reverse=True)
+
+    P = P[:X]
+    Q = Q[:Y]
+    q = P + Q + R
+    q.sort(reverse=True)
+
+    return sum(q[:X + Y])
+
+
+X, Y, A, B, C = map(int, input().split())
+P = list(map(int, input().split()))
+Q = list(map(int, input().split()))
+R = list(map(int, input().split()))
+
+red_and_green_apples(X, Y, A, B, C, P, Q, R)
+
+# red_and_green_apples(
+#     1, 2, 2, 2, 1,
+#     [2, 4],
+#     [5, 1],
+#     [3],
+# )
+#
+# red_and_green_apples(
+#     2, 2, 2, 2, 2,
+#     [8, 6],
+#     [9, 1],
+#     [2, 1],
+# )
