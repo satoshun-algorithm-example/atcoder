0924ac6fd63d765d049085acfab1669554bb01b0
SatoShun <shun.sato1@gmail.com>
2020-05-28 13:10:50 +0000

    sum of large numbers

--- abc163/d.py
+++ abc163/d.py
@@ -7,7 +7,10 @@ res = 1
 s = [i for i in range(N)]
 
 for i in range(K, N):
-    c = set(map(sum, combinations(s, i)))
-    res += len(c)
+    # c = set(map(sum, combinations(s, i)))
+    # res += len(c)
+    minimum = sum(s[:i])
+    maximum = sum(s[-i:])
+    res += (maximum - minimum + 1)
 
 print(res)
