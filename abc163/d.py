287ef5df0b86c7f2a5b3c4b5e206fb69f538b2f4
SatoShun <shun.sato1@gmail.com>
2020-05-29 13:03:11 +0000

    sum of large numbers

--- abc163/d.py
+++ abc163/d.py
@@ -1,14 +1,24 @@
 N, K = map(int, input().split())
 N += 1
 
-res = 1
+res = 0
 s = [i for i in range(N)]
 
-minimum = sum(s[:K-1])
-maximum = sum(s[-K+1:])
+if K == 1:
+    res += N
+    K = 2
+
+if N == 0:
+    print(res)
+    exit(0)
+
+res += 1
+minimum = sum(s[:K - 1])
+maximum = sum(s[-K + 1:])
+
 for i in range(K, N):
     minimum += s[i]
-    maximum += s[-i+1]
-    res = (res + (maximum - minimum + 1)) % (10**9 + 7)
+    maximum += s[-i + 1]
+    res = (res + (maximum - minimum + 1)) % (10 ** 9 + 7)
 
 print(res)
