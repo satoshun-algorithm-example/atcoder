70915fb82f86fff92290b8fa67de421629bee74c
SatoShun <shun.sato1@gmail.com>
2020-05-29 12:50:33 +0000

    sum of large numbers

--- abc163/d.py
+++ abc163/d.py
@@ -1,16 +1,14 @@
-from itertools import combinations
-
 N, K = map(int, input().split())
 N += 1
 
 res = 1
 s = [i for i in range(N)]
 
+minimum = sum(s[:K-1])
+maximum = sum(s[-K+1:])
 for i in range(K, N):
-    # c = set(map(sum, combinations(s, i)))
-    # res += len(c)
-    minimum = sum(s[:i])
-    maximum = sum(s[-i:])
-    res += (maximum - minimum + 1)
+    minimum += s[i]
+    maximum += s[-i+1]
+    res = (res + (maximum - minimum + 1)) % (10**9 + 7)
 
 print(res)
