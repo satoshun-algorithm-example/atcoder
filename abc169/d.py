f1f47dc28b72ae98b23821c0b85a05e4c67d0857
SatoShun <shun.sato1@gmail.com>
2020-05-31 12:26:37 +0000

    d

--- abc169/d.py
+++ abc169/d.py
@@ -1,1 +1,23 @@
+from math import sqrt
+
 N = int(input())
+
+res = 0
+for i in range(2, int(sqrt(N)) + 1):
+    a = set()
+    b = 1
+    while True:
+        if N % i == 0:
+            N = N // i
+            b *= i
+            if b not in a:
+                a.add(b)
+                res += 1
+                b = 1
+        else:
+            break
+
+if N != 1:
+    res += 1
+
+print(res)
