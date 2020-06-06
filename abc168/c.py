253cd5c3d463ca1520d66575d354464287de9155
SatoShun <shun.sato1@gmail.com>
2020-06-06 01:01:29 +0000

    c

--- abc168/c.py
+++ abc168/c.py
@@ -1,3 +1,16 @@
+import math
+
 A, B, H, M = map(int, input().split())
 
+tm = M / 60.0 * 2 * math.pi
+mx = B * math.cos(tm)
+my = B * math.sin(tm)
 
+th = (H * 60.0 + M) / 720 * 2 * math.pi
+hx = A * math.cos(th)
+hy = A * math.sin(th)
+
+dx = hx - mx
+dy = hy - my
+
+print(math.sqrt(dx * dx + dy * dy))
