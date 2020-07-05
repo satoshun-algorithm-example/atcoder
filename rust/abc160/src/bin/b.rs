022888bf713ef05f80fe4449a0f6fdd4584f4ef8
SatoShun <shun.sato1@gmail.com>
2020-07-05 10:33:13 +0000

    abc160 b

--- rust/abc160/src/bin/b.rs
+++ rust/abc160/src/bin/b.rs
@@ -1,3 +1,16 @@
 fn main() {
-    unimplemented!();
+    let mut x = {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        s.trim_end().to_owned().parse::<i32>().unwrap()
+    };
+
+    let mut result = 0;
+
+    result += (x / 500) * 1000;
+    x = x % 500;
+
+    result += (x / 5) * 5;
+
+    println!("{}", result);
 }
