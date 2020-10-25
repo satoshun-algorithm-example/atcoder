f6a6675f1fc630a28a6cec10129a3510433bbf92
SatoShun <shun.sato1@gmail.com>
2020-10-25 03:12:57 +0000

    a * b * c

--- abc179/c.py
+++ abc179/c.py
@@ -2,11 +2,6 @@ N = int(input())
 
 result = 0
 for a in range(1, N):
-    for b in range(1, N):
-        r = a * b
-        if r >= N:
-            break
-
-        result += 1
+    result += (N - 1) // a
 
 print(result)
