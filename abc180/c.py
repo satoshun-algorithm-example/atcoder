0a44099b99fd27c4fed44a5dd97470cf7649495b
SatoShun <shun.sato1@gmail.com>
2020-11-01 08:37:53 +0000

    cream puff

--- abc180/c.py
+++ abc180/c.py
@@ -1,34 +1,11 @@
-from itertools import combinations
-import math
-
-
-def prime_factors(n):
-    if n < 2:
-        return []
-
-    factors = []
-    for p in range(2, int(n ** 0.5) + 1):
-        while n % p == 0:
-            factors.append(p)
-            n //= p
-
-    if n > 1:
-        factors.append(n)
-
-    return factors
-
-
 N = int(input())
 
-factors = prime_factors(N)
+ans = []
 
-result = {1}
+for i in range(1, int(N ** 0.5) + 1):
+    if N % i == 0:
+        ans.append(i)
+        ans.append(N // i)
 
-for i in range(1, len(factors) + 1):
-    for j in combinations(factors, i):
-        result.add(math.prod(j))
-
-result = sorted(list(result))
-
-for r in result:
-    print(r)
+for i in sorted(ans):
+    print(i)
