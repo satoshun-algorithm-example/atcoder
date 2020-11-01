98deb4f10be782b1f4f1c2d9fde9ce87e4177259
SatoShun <shun.sato1@gmail.com>
2020-11-01 07:55:08 +0000

    c

--- abc180/c.py
+++ abc180/c.py
@@ -1,5 +1,34 @@
+from itertools import combinations
+import math
+
+
+def prime_factors(n):
+    if n < 2:
+        return []
+
+    factors = []
+    for p in range(2, int(n ** 0.5) + 1):
+        while n % p == 0:
+            factors.append(p)
+            n //= p
+
+    if n > 1:
+        factors.append(n)
+
+    return factors
+
+
 N = int(input())
 
-for i in range(1, N + 1):
-    if N % i == 0:
-        print(i)
+factors = prime_factors(N)
+
+result = {1}
+
+for i in range(1, len(factors) + 1):
+    for j in combinations(factors, i):
+        result.add(math.prod(j))
+
+result = sorted(list(result))
+
+for r in result:
+    print(r)
