15fa57b7728a906b3b87da469a30b18c305bf9ea
SatoShun <shun.sato1@gmail.com>
2020-11-01 08:41:08 +0000

    cream puff

--- abc180/c.py
+++ abc180/c.py
@@ -1,11 +1,13 @@
+import heapq
+
 N = int(input())
 
 ans = []
 
 for i in range(1, int(N ** 0.5) + 1):
     if N % i == 0:
-        ans.append(i)
-        ans.append(N // i)
+        heapq.heappush(ans, i)
+        heapq.heappush(ans, N // i)
 
-for i in sorted(ans):
+for i in ans:
     print(i)
